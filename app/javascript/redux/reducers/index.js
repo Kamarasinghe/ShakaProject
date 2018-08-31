361e9d24b3f13a6173bc96e4b66bde4c9c91263d
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-08-30 19:59:17 -0700

    Intialized root reducer

---
+++ app/javascript/redux/reducers/index.js
@@ -0,0 +1,20 @@
+/*
+  Reducers produce the state of the application
+  Reducers take in two first parameters: initial state and an action
+  Next state is calculated based on the action type
+  Should return the initial state if no matching actions
+*/
+
+const initialState = {
+  username: '',
+  password: ''
+};
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default: 
+    return state;
+  }
+};
+
+export default rootReducer;
