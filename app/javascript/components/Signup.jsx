54359f12ec5241fab78b3694f5e745b851e144c6
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-08-31 11:36:04 -0700

    Created the signup modal and connected toggle function from App

---
+++ app/javascript/components/Signup.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+class Signup extends Component {
+  render(props) {
+    return (
+      <div>
+        <Modal isOpen={true}>
+          <ModalHeader>Create an Account</ModalHeader>
+          <ModalBody>
+            TODO: Create form
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={'TODO'}>Register</Button>{' '}
+            <Button color="secondary" onClick={() => { this.props.toggle('signup') }}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
+};
+
+export default Signup;
