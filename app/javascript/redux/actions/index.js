23246b68b7b5a29f15659b6af995c4c2102c1dba
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-08-30 20:04:28 -0700

    Created action file with comments

---
+++ app/javascript/redux/actions/index.js
@@ -0,0 +1,6 @@
+/*
+  'Dispatch an action' in order to change the state
+  Actions require a 'type' property to describe how to change state
+  An action can take in a 'payload' which is potentially what is going to be
+  added/changed in the state
+*/
