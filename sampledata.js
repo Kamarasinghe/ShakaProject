719154c3592d939af6903c176e564b9ff11860bd
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-08-31 15:36:14 -0700

    Created sample users with messages to emulate data

---
+++ sampledata.js
@@ -0,0 +1,104 @@
+const usersAndMessages = [
+  {
+    first: 'Brooklynn',
+    last: 'Wilcox',
+    username: 'bentleykindly',
+    message: 'Don’t go through life. GROW through life.',
+    createdAt: '1535753956060',
+  },
+  {
+    first: 'Kierra',
+    last: 'Cabrera',
+    username: 'cleoluxuriant',
+    message: 'Turning up the volume is like zooming in, but with sound.',
+    createdAt: '1535754234352',
+  },
+  {
+    first: 'Dominique',
+    last: 'Stein',
+    username: 'cooperabiding',
+    message: 'Typo’s aren’t my biggest problem – Thinko’s are.',
+    createdAt: '1535754307090',
+  },
+  {
+    first: 'Korbin',
+    last: 'Kane',
+    username: 'marleyadvanced',
+    message: 'I have something to say, it’s better to die young, than to fade away.',
+    createdAt: '1535754376117',
+  },
+  {
+    first: 'Dayana',
+    last: 'Fernandez',
+    username: 'milliequack',
+    message: 'Let your day be neither manic nor volcanic.',
+    createdAt: '1535754452943',
+  },
+  {
+    first: 'Jayleen',
+    last: 'Shannon',
+    username: 'roxyfond',
+    message: 'Out of my mind. Back in five minutes.',
+    createdAt: '1535754566361',
+  },
+  {
+    first: 'Michael',
+    last: 'Williams',
+    username: 'ravendoting',
+    message: 'A hangover is the wrath of grapes.',
+    createdAt: '1535754614122',
+  },
+  {
+    first: 'Talon',
+    last: 'Cummings',
+    username: 'tangoavaricious',
+    message: 'Attitude is infectious… Is yours worth catching?',
+    createdAt: '1535754614122',
+  },
+  {
+    first: 'Rene',
+    last: 'Irwin',
+    username: 'otisgrave',
+    message: 'I don’t suffer from insanity, I enjoy every minute of it.',
+    createdAt: '1535754724853',
+  },
+  {
+    first: 'Jordyn',
+    last: 'Odom',
+    username: 'zeusmedical',
+    message: 'Two wrongs don’t make a right, but three lefts do.',
+    createdAt: '1535754774205',
+  },
+  {
+    first: 'Jazlynn',
+    last: 'Castaneda',
+    username: 'rumorthrifty',
+    message: 'OK, so what’s the speed of dark?',
+    createdAt: '1535754822910',
+  },
+  {
+    first: 'Kierra',
+    last: 'Cabrera',
+    username: 'cleoluxuriant',
+    message: 'Depression is merely anger without enthusiasm.',
+    createdAt: '1535754566361',
+  },
+  {
+    first: 'Michael',
+    last: 'Williams',
+    username: 'ravendoting',
+    message: 'I intend to live forever – so far, so good.',
+    createdAt: '1535754882512',
+  },
+  {
+    first: 'Dayana',
+    last: 'Fernandez',
+    username: 'milliequack',
+    message: 'I used to have an open mind but my brains kept falling out.',
+    createdAt: '1535754913087',
+  },
+]
+
+module.exports = {
+  usersAndMessages
+};
