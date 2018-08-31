ce68f88746f8ee5efe28adb1d369a71b1b59d789
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-08-31 15:11:39 -0700

    Implemented tests for functionality of sign up and sign in buttons

--- app/javascript/test/App.test.js
+++ app/javascript/test/App.test.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { configure, shallow, mount } from 'enzyme';
-import App from '../components/App';
+import { App } from '../components/App';
+import store from '../redux/store'
+
 
 describe ('The app component', () => {
-  const wrapper = shallow(<App />);
+  const wrapper = shallow(<App admin={true} />);
 
   it ('should exist', () => {
     expect(wrapper.exists());
+  });
+
+  it ('click on sign up should set signup state to true', () => {
+    wrapper.find('.signUp').simulate('click');
+    expect(wrapper.state().signup).toBe(true);
+  });
+  
+  it ('click on sign in should set signin state to true', () => {
+    wrapper.find('.signIn').simulate('click');
+    expect(wrapper.state().signin).toBe(true);
   });
 });
