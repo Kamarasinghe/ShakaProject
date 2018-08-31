18c2a009a5459d8b358f0b9573f45a30396bf091
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-08-31 16:28:28 -0700

    Added input fields for Username and Password

--- app/javascript/components/SignIn.jsx
+++ app/javascript/components/SignIn.jsx
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Button, 
+  Modal, 
+  ModalHeader, 
+  ModalBody, 
+  ModalFooter,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Input 
+} from 'reactstrap';
 
 class SignIn extends Component {
-  render(props) {
+  render() {
     return (
       <div>
         <Modal isOpen={true}>
           <ModalHeader>Log Into Account</ModalHeader>
           <ModalBody>
-            TODO: Login form
+            <form onSubmit={'doSomething'}>
+              <InputGroup size="lg">
+                <InputGroupAddon addonType="prepend">Username</InputGroupAddon>
+                <Input name='username' />
+              </InputGroup>
+              <InputGroup size="lg">
+                <InputGroupAddon addonType="prepend">Password</InputGroupAddon>
+                <Input name='password' />
+              </InputGroup>
+            </form>
           </ModalBody>
           <ModalFooter>
-            <Button color="primary" onClick={'TODO'}>Log In</Button>{' '}
+            <Button color="primary" onClick={'TODO'}>Log In</Button>
             <Button color="secondary" onClick={() => { this.props.toggle('login') }}>Cancel</Button>
           </ModalFooter>
         </Modal>
