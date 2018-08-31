ac56ca97f7bdb490727fb6bfe2b858e1c0fa1e64
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-08-31 11:35:10 -0700

    Created toggleModal funcationality, imported Modal component, and added conditional rendering

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'; // Connects component to Redux
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import Signup from './Signup';
 
 // Used to grab what you need from the store and pass to component
 const mapStateToProps = (state) => {
@@ -13,6 +14,25 @@ const mapStateToProps = (state) => {
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      signup: false,
+      register: false
+    }
+
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+
+  // Handles opening and closing of both register and login modals
+  toggleModal (modal) {
+    (modal === 'signup') ? (
+      this.setState({
+        signup: !this.state.signup
+      })
+    ) : (
+      this.setState({
+        register: !this.state.register
+      })
+    )
   }
 
   render() {
@@ -22,16 +42,17 @@ class App extends Component {
           <NavbarBrand href='/'>ShakaProject</NavbarBrand>
             <Nav className='ml-auto' navbar>
               <NavItem>
-                <NavLink href=''>Sign Up</NavLink>
+                <NavLink onClick={ () => { this.toggleModal('signup') } }>Sign Up</NavLink>
               </NavItem>
               <NavItem>
                 <NavLink href=''>Register</NavLink>
               </NavItem>
             </Nav>
         </Navbar>
+        {this.state.signup ? (<Signup toggle={this.toggleModal} />) : <div></div>}
       </div>
-    )
-  };
+    );
+  }
 };
 
 App = connect(mapStateToProps)(App);
