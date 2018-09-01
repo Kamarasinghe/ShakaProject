51a07a2bb1c1a8b3a3a2a0c93b9c50831209562d
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-09-01 09:19:56 -0700

    Added classes to login and input fields

--- app/javascript/components/SignIn.jsx
+++ app/javascript/components/SignIn.jsx
@@ -17,16 +17,16 @@ class SignIn extends Component {
           <ModalHeader>Log Into Account</ModalHeader>
           <ModalBody>
             <InputGroup size='lg'>
-              <InputGroupAddon addonType='prepend'>Username</InputGroupAddon>
+              <InputGroupAddon addonType='prepend' className='username'>Username</InputGroupAddon>
               <Input name='username' />
             </InputGroup>
             <InputGroup size='lg'>
-              <InputGroupAddon addonType='prepend'>Password</InputGroupAddon>
+              <InputGroupAddon addonType='prepend' className='password'>Password</InputGroupAddon>
               <Input name='password' />
             </InputGroup>
           </ModalBody>
           <ModalFooter>
-            <Button color='primary' onClick={'TODO'}>Log In</Button>
+            <Button color='primary' className='login' onClick={'TODO'}>Log In</Button>
             <Button color='secondary' onClick={() => { this.props.toggle('login') }}>Cancel</Button>
           </ModalFooter>
         </Modal>
