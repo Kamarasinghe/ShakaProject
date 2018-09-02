11d449215066abb3b17c551b07b6cdb4bc8bb62f
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-09-02 13:11:28 -0700

    Sending all messages and all users via json

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,6 +1,9 @@
 class MessageController < ApplicationController
   def index
+    # @messages = Message.joins(:user)
+    # render :json => @messages
     @messages = Message.all
-    render :json => @messages
+    @users = User.all
+    render :json => [ @messages, @users ]
   end
 end
