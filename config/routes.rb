0e7f9267bbb9a2390efa4a8c3370f56761a1b2f9
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-09-03 12:57:20 -0700

    Created delete route to logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  delete '/logout' => 'sessions#destroy'
   post '/login' => 'sessions#create'
-  get '/users' => 'users#index'
   get '/messages' => 'message#index'
+  get '/users' => 'users#index'
   post '/signup' => 'users#create'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
