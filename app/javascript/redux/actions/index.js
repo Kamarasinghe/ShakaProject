fef8d7b21b7d9cc6f16b70e020d62c9d1ad21d41
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-09-02 22:13:56 -0700

    Created getMessages action

--- app/javascript/redux/actions/index.js
+++ app/javascript/redux/actions/index.js
@@ -4,3 +4,8 @@
   An action can take in a 'payload' which is potentially what is going to be
   added/changed in the state
 */
+
+export const getMessages = (messages) => ({
+  type: 'GET_MESSAGES', 
+  payload: messages
+});
