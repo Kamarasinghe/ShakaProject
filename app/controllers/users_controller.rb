f99f70ec5d199ab2dd6e169543e45a577aefc98d
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-09-03 12:26:02 -0700

    Added isAdmin to params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,6 @@ class UsersController < ApplicationController
 
   private 
     def user_params
-      params.require(:user).permit(:first, :username, :email, :password)
+      params.require(:user).permit(:first, :username, :email, :password, :isAdmin)
     end
 end
