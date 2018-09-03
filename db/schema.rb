c81bd95e16e5667f3208a4fad61a2fc29554b553
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-09-03 12:12:29 -0700

    Added isAdmin boolean to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_02_162220) do
+ActiveRecord::Schema.define(version: 2018_09_03_190847) do
 
   create_table "messages", force: :cascade do |t|
     t.string "message"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2018_09_02_162220) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "isAdmin"
   end
 
 end
