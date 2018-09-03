45d0334164e96430091ad7f80b2df56ccf0417ac
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-09-02 21:27:56 -0700

    Routing to signup, User controller create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get '/users' => 'users#index'
   get '/messages' => 'message#index'
-  post 'signup' => 'users#create'
+  post '/signup' => 'users#create'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
