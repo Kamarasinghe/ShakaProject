fc04fc45a4d0f81fa4d2a9350ba935cfeb0ed243
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-09-03 22:54:54 -0700

    Created private message_params method and create method

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,6 +1,23 @@
 class MessageController < ApplicationController
+  protect_from_forgery with: :null_session
+
   def index
-    @messages = Message.all
+    @messages = Message.all.order('created_at DESC')
     render json: @messages, :include => [:user]
   end
+
+  def create
+    @message = Message.new(message_params)
+
+    if @message.save
+      render :json => 'Success'
+    else
+      render :json => 'Failed'
+    end
+  end
+
+  private 
+    def message_params
+      params.require(:messages).permit(:message, :user_id)
+    end
 end
