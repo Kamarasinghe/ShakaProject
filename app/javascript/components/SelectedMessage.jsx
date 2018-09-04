4fe61e848191ab428ae8548c190a67d56d3cbaff
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-09-04 11:00:04 -0700

    Modal that renders original message and input for updated message

---
+++ app/javascript/components/SelectedMessage.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { 
+  Button, 
+  Modal, 
+  ModalHeader, 
+  ModalBody, 
+  ModalFooter,
+  Form,
+  FormGroup,
+  Label,
+  Input
+} from 'reactstrap';
+
+class SelectedMessage extends Component {
+  render() {
+    return (
+      <div>
+          <Modal isOpen={true}>
+            <ModalHeader>Edit Message</ModalHeader>
+            <ModalBody>
+              <Label for='firstName' style={{paddingRight: '5px'}}>Original:</Label>
+              {this.props.selectedMessage.message}
+              <br />
+              <Form>
+                <FormGroup style={{paddingTop: '15px'}}>
+                  <Input type='text' name='message' className='editMessage' onChange={this.props.handleChange} value={this.props.message} />
+                </FormGroup>
+              </Form>
+            </ModalBody>
+            <ModalFooter>
+              <Button color='primary' className='update' onClick={() => { this.props.handleSubmit(this.props.selectedMessage.id) }}>Update</Button>
+              <Button color='secondary' onClick={() => { this.props.toggle() }}>Cancel</Button>
+            </ModalFooter>
+          </Modal>
+      </div>
+    );
+  }
+};
+
+export default SelectedMessage;
