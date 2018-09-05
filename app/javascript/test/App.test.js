f7962bda90705d042187b4f72db05dd2f2f0c2f8
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-09-04 20:57:01 -0700

    Added a few more tests for the App component

--- app/javascript/test/App.test.js
+++ app/javascript/test/App.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { configure, shallow, mount } from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import { App } from '../components/App';
-import store from '../redux/store'
 
 
 describe ('The app component', () => {
   const wrapper = shallow(<App admin={true} />);
+  const instance = wrapper.instance();
 
   it ('should exist', () => {
     expect(wrapper.exists());
@@ -19,5 +19,17 @@ describe ('The app component', () => {
   it ('click on sign in should set signin state to true', () => {
     wrapper.find('.signIn').simulate('click');
     expect(wrapper.state().signin).toBe(true);
+  });
+
+  it ('handleChange should change state depending on input', () => {
+    instance.handleChange({ target : { name: 'first', value: 'Test' }});
+    instance.handleChange({ target : { name: 'email', value: 'test@test.com' }});
+    instance.handleChange({ target : { name: 'username', value: 'testing' }});
+    instance.handleChange({ target : { name: 'password', value: 'asdf' }});
+
+    expect(wrapper.state().first).toBe('Test');
+    expect(wrapper.state().email).toBe('test@test.com');
+    expect(wrapper.state().username).toBe('testing');
+    expect(wrapper.state().password).toBe('asdf');
   });
 });
